0cf4b27205539dd994f3b0dd8f9e5ec75ce3946f
oskar <oskar@LAPTOP-KCDS3GB3.lan>
2020-06-03 12:31:38 +0200

    Fix lägg till alien

--- src/realisering/HuvudmenyAlien.java
+++ src/realisering/HuvudmenyAlien.java
@@ -1,8 +1,7 @@
 package realisering;
 
-import javax.swing.JOptionPane;
-import oru.inf.InfException;
-import oru.inf.InfDB;
+import javax.swing.*;
+import oru.inf.*;
 
 /**
  * Klassen förser användaren, i det här fallet en alien, med ett gränssnitt
@@ -36,6 +35,7 @@ public class HuvudmenyAlien extends javax.swing.JFrame {
         lblAlienOmrade = new javax.swing.JLabel();
         lblOmradeschef = new javax.swing.JLabel();
         Tillbaka = new javax.swing.JButton();
+        andraLosenord = new javax.swing.JButton();
 
         btnTillbakaInlogg.setText("Tillbaka till inlogg");
         btnTillbakaInlogg.addActionListener(new java.awt.event.ActionListener() {
@@ -64,39 +64,62 @@ public class HuvudmenyAlien extends javax.swing.JFrame {
             }
         });
 
+        andraLosenord.setText("Ändra lösenord");
+        andraLosenord.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                andraLosenordMouseClicked(evt);
+            }
+        });
+        andraLosenord.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                andraLosenordActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(22, 22, 22)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(lblOmradeschef, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(lblAlienOmrade, javax.swing.GroupLayout.PREFERRED_SIZE, 182, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(lblOmradeTillhorighet))
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(lblinloggNamn, javax.swing.GroupLayout.PREFERRED_SIZE, 182, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(94, 94, 94)
-                        .addComponent(Tillbaka, javax.swing.GroupLayout.PREFERRED_SIZE, 105, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addGap(65, 65, 65))
-            .addGroup(layout.createSequentialGroup()
                 .addGap(146, 146, 146)
                 .addComponent(lblHuvudmenyAlien)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(22, 22, 22)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(lblOmradeschef, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGap(174, 174, 174))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(lblAlienOmrade, javax.swing.GroupLayout.PREFERRED_SIZE, 182, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(lblOmradeTillhorighet))
+                                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                        .addComponent(andraLosenord))))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(lblinloggNamn, javax.swing.GroupLayout.PREFERRED_SIZE, 182, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(94, 94, 94)
+                                .addComponent(Tillbaka, javax.swing.GroupLayout.PREFERRED_SIZE, 105, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addContainerGap(65, Short.MAX_VALUE))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addGap(27, 27, 27)
                 .addComponent(lblHuvudmenyAlien, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 54, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 49, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(lblinloggNamn)
                     .addComponent(Tillbaka))
-                .addGap(13, 13, 13)
-                .addComponent(lblAlienOmrade)
+                .addGap(9, 9, 9)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lblAlienOmrade)
+                    .addComponent(andraLosenord))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(lblOmradeTillhorighet)
                 .addGap(15, 15, 15)
@@ -184,8 +207,18 @@ public class HuvudmenyAlien extends javax.swing.JFrame {
         //Återvänder till Login-fönstret
     }//GEN-LAST:event_TillbakaActionPerformed
 
+    private void andraLosenordActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_andraLosenordActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_andraLosenordActionPerformed
+
+    private void andraLosenordMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_andraLosenordMouseClicked
+        new AndraLosenord(mib).setVisible(true);
+        dispose();
+    }//GEN-LAST:event_andraLosenordMouseClicked
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton Tillbaka;
+    private javax.swing.JButton andraLosenord;
     private javax.swing.JButton btnTillbakaInlogg;
     private javax.swing.JLabel lblAlienOmrade;
     private javax.swing.JLabel lblHuvudmenyAlien;
